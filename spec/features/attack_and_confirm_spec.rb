263aeb22b851a7ea9566062e8408553cabce88bd
Will Wayman <will.jdw@gmail.com>
2018-11-14 15:55:59 +0000

    new route for attack

--- spec/features/attack_and_confirm_spec.rb
+++ spec/features/attack_and_confirm_spec.rb
@@ -2,6 +2,6 @@ feature "Attack other player" do
   scenario "Player 1 attacks Player 2 and attack confirmed" do
     sign_in_and_play()
     click_button 'Attack'
-    expect(page).to have_text("Joe: 70HP")
+    expect(page).to have_text("Will attacked Joe")
   end
 end
