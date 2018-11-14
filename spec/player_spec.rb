3b7af96ff14a5df04844509563c029a33a5344bb
Will Wayman <will.jdw@gmail.com>
2018-11-14 17:00:47 +0000

    unit test for attack reducing HP

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,26 @@
 require 'player'
 
 describe Player do
+  subject(:will) { Player.new('Will') }
+  subject(:joe) { Player.new('Joe') }
+
   describe "#name" do
     it "should return the players name" do
-      p1 = described_class.new("Will")
-      expect(p1.name).to eq("Will")
+      expect(will.name).to eq("Will")
     end
   end
+
+  describe '#hitpoints' do
+    it 'should show the default hitpoints' do
+      expect(will.hitpoints).to eq 100
+    end
+  end
+
+  describe '#attack' do
+    it 'shoud reduce the hitpoints by 10' do
+      will.attack(joe)
+      expect(joe.hitpoints).to eq 90
+    end
+  end
+
 end
